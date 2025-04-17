1234196f763ca43913ca267e4bcfb5c76acade74
Neha Devarapalli <devarapalli.n@northeastern.edu>
2025-04-17 15:49:26 -0400

    final

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+      "@/": path.resolve(__dirname, "src"),
       "@/components": path.resolve(__dirname, "src/components"),
       "@/pages": path.resolve(__dirname, "src/pages"),
       "@/styles": path.resolve(__dirname, "src/styles"),
